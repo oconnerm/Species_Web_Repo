c7cff699298a93a19578546634ab17778c79cef4
Ryan <oconnerm@gmail.com>
2017-03-18 20:26:54 -0600

    Field gude updates

--- species-website/public/assets/js/fieldguide_data.js
+++ species-website/public/assets/js/fieldguide_data.js
@@ -24,6 +24,26 @@ window.onclick = function(event) {
 
   document.body.style.backgroundColor = "#384452";
 
+  //alert(display_species);
+
+/*
+
+          <h2 id="display_species" style="visibility: hidden;">Achillea millefolium<h2>
+          <h2 id="display_common_name" style="visibility: hidden;">Garret common yarrow<h2>
+          <h2 id="display_family_name" style="visibility: hidden;">Asteraceae<h2>
+          <h2 id="display_synonyms" style="visibility: hidden;">Achillea lanulosa<h2>
+          <h3 id="display_growth_form" style="visibility: hidden;">forb<h2>
+          <h3 id="display_folower_color" style="visibility: hidden;">white<h2>
+          <h3 id="display_flower_shape" style="visibility: hidden;">composite<h2>
+          <h3 id="display_habitat" style="visibility: hidden;">dry meadow and wet meadow<h2>
+          <h3 id="display_leaf_arrangment" style="visibility: hidden;">alternate<h2>
+          <h3 id="display_leaf_shape" style="visibility: hidden;">other<h2>
+          <h3 id="display_petal_number" style="visibility: hidden;">NA<h2>
+          <h4 id="display_notes" style="visibility: hidden;">NA<h2>
+          <h4 id="display_photo_credit" style="visibility: hidden;">JG Smith<h2>
+
+*/
+
   var forbs_query = firebase.database().ref("speciesid/field_guide/forbs").orderByKey();
   forbs_query.once("value")
     .then(function(snapshot) {
@@ -31,8 +51,19 @@ window.onclick = function(event) {
 
         var speciesName = childSnapshot.val().species_name;
         var commonName = childSnapshot.val().common_name;
+        var familyName = childSnapshot.val().family_name;
+        var synonymsName = childSnapshot.val().synonyms;
+        var growthForm = childSnapshot.val().growth_form;
+        var flowerColor = childSnapshot.val().flower_color;
+        var flowerShape = childSnapshot.val().flower_shape;
+        var habitatName = childSnapshot.val().habitat;
+        var leafArrangement = childSnapshot.val().leaf_arrangement;
+        var leafShapeFilter = childSnapshot.val().leaf_shape_filter;
+        var petalNumber = childSnapshot.val().petal_number;
+        var notesName = childSnapshot.val().notes;
+        var photoCredit = childSnapshot.val().photo_credit;
 
-        //console.log(species_name);
+        //var plantCode = getParent(childSnapshot);
 
         var h6 = document.createElement('h6');
         var text = document.createTextNode(speciesName);
@@ -40,7 +71,33 @@ window.onclick = function(event) {
         document.body.appendChild(h6);
 
         h6.onclick = function () {
-          alert(speciesName);
+          var display_species = parent.document.getElementById("display_species");
+          var display_common_name = parent.document.getElementById("display_common_name");
+          var display_family_name = parent.document.getElementById("display_family_name");
+          var display_synonyms = parent.document.getElementById("display_synonyms");
+          var display_growth_form = parent.document.getElementById("display_growth_form");
+          var display_folower_color = parent.document.getElementById("display_folower_color");
+          var display_flower_shape = parent.document.getElementById("display_flower_shape");
+          var display_habitat = parent.document.getElementById("display_habitat");
+          var display_leaf_arrangement = parent.document.getElementById("display_leaf_arrangement");
+          var display_leaf_shape = parent.document.getElementById("display_leaf_shape");
+          var display_petal_number = parent.document.getElementById("display_petal_number");
+          var display_notes = parent.document.getElementById("display_notes");
+          var display_photo_credit = parent.document.getElementById("display_photo_credit");
+
+          display_species.textContent = speciesName;
+          display_common_name.textContent = commonName;
+          display_family_name.textContent = familyName;
+          display_synonyms.textContent = synonymsName;
+          display_growth_form.textContent = growthForm;
+          display_folower_color.textContent = flowerColor;
+          display_flower_shape.textContent = flowerShape;
+          display_habitat.textContent = habitatName;
+          display_leaf_arrangement.textContent = leafArrangement;
+          display_leaf_shape.textContent = leafShapeFilter;
+          display_petal_number.textContent = petalNumber;
+          display_notes.textContent = notesName;
+          display_photo_credit.textContent = photoCredit;          
         };
 
         var h5 = document.createElement('h5');
@@ -68,7 +125,8 @@ window.onclick = function(event) {
         document.body.appendChild(h6);
 
         h6.onclick = function () {
-          alert(speciesName);
+          var display_species = parent.document.getElementById("display_species");
+          display_species.textContent = speciesName;
         };
 
         var h5 = document.createElement('h5');
@@ -96,7 +154,8 @@ window.onclick = function(event) {
         document.body.appendChild(h6);
 
         h6.onclick = function () {
-          alert(speciesName);
+          var display_species = parent.document.getElementById("display_species");
+          display_species.textContent = speciesName;
         };
 
         var h5 = document.createElement('h5');
@@ -124,7 +183,8 @@ window.onclick = function(event) {
         document.body.appendChild(h6);
 
         h6.onclick = function () {
-          alert(speciesName);
+          var display_species = parent.document.getElementById("display_species");
+          display_species.textContent = speciesName;
         };
 
         var h5 = document.createElement('h5');
@@ -152,7 +212,8 @@ window.onclick = function(event) {
         document.body.appendChild(h6);
 
         h6.onclick = function () {
-          alert(speciesName);
+          var display_species = parent.document.getElementById("display_species");
+          display_species.textContent = speciesName;
         };
 
         var h5 = document.createElement('h5');
@@ -180,7 +241,8 @@ window.onclick = function(event) {
         document.body.appendChild(h6);
 
         h6.onclick = function () {
-          alert(speciesName);
+          var display_species = parent.document.getElementById("display_species");
+          display_species.textContent = speciesName;
         };
 
         var h5 = document.createElement('h5');
--- species-website/public/assets/js/fieldguide_display.js
+++ species-website/public/assets/js/fieldguide_display.js
@@ -24,11 +24,98 @@ window.onclick = function(event) {
 
   document.body.style.backgroundColor = "#384452";
 
-/*
-  var h2 = document.createElement('h2');
-  var text = document.createTextNode("speciesName");
-  h2.appendChild(text);
-  document.body.appendChild(h2);
-  */
+  // TO DO: Move this to setInterval function
+
+  var storage = firebase.storage();
+  var storageRef = storage.ref();
+  var tangRef = storageRef.child('field_guide/flower.jpg');
+
+  tangRef.getDownloadURL().then(function(url) {
+    document.querySelector('img').src = url;    
+  })
+
 
 }());
+
+setInterval(function (){
+  //var display = document.getElementById('display');
+
+  var display_species2 = document.getElementById("display_species");
+  var display_common_name2 = document.getElementById("display_common_name");
+  var display_family_name2 = document.getElementById("display_family_name");
+  var display_synonyms2 = document.getElementById("display_synonyms");
+  var display_growth_form2 = document.getElementById("display_growth_form");
+  var display_folower_color2 = document.getElementById("display_folower_color");
+  var display_flower_shape2 = document.getElementById("display_flower_shape");
+  var display_habitat2 = document.getElementById("display_habitat");
+  var display_leaf_arrangement2 = document.getElementById("display_leaf_arrangement");
+  var display_leaf_shape2 = document.getElementById("display_leaf_shape");
+  var display_petal_number2 = document.getElementById("display_petal_number");
+  var display_notes2 = document.getElementById("display_notes");
+  var display_photo_credit2 = document.getElementById("display_photo_credit");
+
+  var display_species = parent.document.getElementById("display_species");
+  var display_common_name = parent.document.getElementById("display_common_name");
+  var display_family_name = parent.document.getElementById("display_family_name");
+  var display_synonyms = parent.document.getElementById("display_synonyms");
+  var display_growth_form = parent.document.getElementById("display_growth_form");
+  var display_folower_color = parent.document.getElementById("display_folower_color");
+  var display_flower_shape = parent.document.getElementById("display_flower_shape");
+  var display_habitat = parent.document.getElementById("display_habitat");
+  var display_leaf_arrangement = parent.document.getElementById("display_leaf_arrangement");
+  var display_leaf_shape = parent.document.getElementById("display_leaf_shape");
+  var display_petal_number = parent.document.getElementById("display_petal_number");
+  var display_notes = parent.document.getElementById("display_notes");
+  var display_photo_credit = parent.document.getElementById("display_photo_credit");
+
+  if(display_species2.textContent === display_species.innerHTML){}
+  else
+  {
+    display_species2.textContent = display_species.innerHTML;
+    display_common_name2.textContent = display_common_name.innerHTML;
+    display_family_name2.textContent = display_family_name.innerHTML;
+    display_synonyms2.textContent = display_synonyms.innerHTML;
+    display_growth_form2.textContent = display_growth_form.innerHTML;
+    display_folower_color2.textContent = display_folower_color.innerHTML;
+    display_flower_shape2.textContent = display_flower_shape.innerHTML;
+    display_habitat2.textContent = display_habitat.innerHTML;
+    display_leaf_arrangement2.textContent = display_leaf_arrangement.innerHTML;
+    display_leaf_shape2.textContent = display_leaf_shape.innerHTML;
+    display_petal_number2.textContent = display_petal_number.innerHTML;
+    display_notes2.textContent = display_notes.innerHTML;
+    display_photo_credit2.textContent = display_photo_credit.innerHTML;
+  }
+
+/*
+  var wantedOrder = firebase.database().ref('orders').equalTo("My Input Text");
+  
+  var forbs_query = firebase.database().ref("speciesid/field_guide/forbs").orderByKey();
+
+  forbs_query.once("value")
+    .then(function(snapshot) {
+      snapshot.forEach(function(childSnapshot) {
+
+        var speciesName = childSnapshot.val().species_name;
+        var commonName = childSnapshot.val().common_name;
+
+        //console.log(species_name);
+
+        var h6 = document.createElement('h6');
+        var text = document.createTextNode(speciesName);
+        h6.appendChild(text);
+        document.body.appendChild(h6);
+
+
+        var h5 = document.createElement('h5');
+        var text2 = document.createTextNode(commonName);
+        h5.appendChild(text2);
+        document.body.appendChild(h5);
+
+    });
+
+  });
+  */
+
+  },50);
+
+
